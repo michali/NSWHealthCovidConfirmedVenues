90c7f089503edb95dddd3e8cb1069c1a1ff0f5e9
Michali K <michali@michalikons.com>
2020-12-23 08:15:57 +1100

    Added more cases around updated venues

--- tests/NewLocationsRetrieverTests.ts
+++ tests/NewLocationsRetrieverTests.ts
@@ -63,6 +63,65 @@ describe("New Locations Retriever", function() {
         expect(updates).toHaveSize(1);
         expect(updates[0]).toBe(newLocation);
     });
+
+    it("Second run, if locations removed, return no updates", function() {
+      
+        metadataRetriever.Get.and.returnValue({
+            last_modified: "2020-12-21T02:14:45.944132",
+            url: "https://path.to/url"
+        });
+
+        locationRetriever.Get.withArgs("https://path.to/url").and.returnValue([{
+            name: "venue name 1",
+            address: "address 1"
+        },
+        {
+            name: "venue name 2",
+            address: "address 2"  
+        }]);
+
+        newVenuesRetriever.Get();
+
+        locationRetriever.Get.withArgs("https://path.to/url").and.returnValue([{
+            name: "venue name 1",
+            address: "address 1"
+        }]);
+
+        var updates = newVenuesRetriever.Get();
+
+        expect(updates).toHaveSize(0);
+    });
+
+    it("Second run, if set different, return new set only", function() {
+      
+        metadataRetriever.Get.and.returnValue({
+            last_modified: "2020-12-21T02:14:45.944132",
+            url: "https://path.to/url"
+        });
+
+        locationRetriever.Get.withArgs("https://path.to/url").and.returnValue([{
+            name: "venue name 1",
+            address: "address 1"
+        },
+        {
+            name: "venue name 2",
+            address: "address 2"  
+        }]);
+
+        newVenuesRetriever.Get();
+
+        var newLocation = {
+            name: "venue name 3",
+            address: "address 3"
+        }
+
+        locationRetriever.Get.withArgs("https://path.to/url").and.returnValue([newLocation]);
+
+        var updates = newVenuesRetriever.Get();
+
+        expect(updates).toHaveSize(1);
+        expect(updates[0]).toBe(newLocation);
+    });
   });
       
   
